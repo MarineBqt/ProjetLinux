7879343d3f4a8cd9301745a2b422f3625dcc3bdd
Marine <bouquetmarine@hotmail.fr>
2023-03-18 16:50:13 +0000

    Modification

--- company_data.sh
+++ company_data.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-previous_close=$(curl -s https://fr.finance.yahoo.com/quote/TSLA/ | grep -oP '(?<=data-test="PREV_CLOSE-value">)[\d,]+')
+previous_close=$(curl -s https://fr.finance.yahoo.com/quote/TSLA/ | grep -oP '(?<=data-test="PREV_CLOSE-value">)[\d,]+' | sed 's/,/./')
 
-cap_boursiere=$(curl -s https://fr.finance.yahoo.com/quote/TSLA/ | grep -oP '(?<=data-test="MARKET_CAP-value">)[\d,]+')
+cap_boursiere=$(curl -s https://fr.finance.yahoo.com/quote/TSLA/ | grep -oP '(?<=data-test="MARKET_CAP-value">)[\d,]+' | sed 's/,/./')
 
-ouverture=$(curl -s https://fr.finance.yahoo.com/quote/TSLA/ | grep -oP '(?<=data-test="OPEN-value">)[\d,]+')
+ouverture=$(curl -s https://fr.finance.yahoo.com/quote/TSLA/ | grep -oP '(?<=data-test="OPEN-value">)[\d,]+' | sed 's/,/./')
 
-volume=$(curl -s https://fr.finance.yahoo.com/quote/TSLA/ | grep -oP '(?<=data-test="AVERAGE_VOLUME_3MONTH-value">)[\d, ]+')
+volume=$(curl -s https://fr.finance.yahoo.com/quote/TSLA/ | grep -oP '(?<=data-test="AVERAGE_VOLUME_3MONTH-value">)[\d, ]+' | sed 's/,/./')
 
-target=$(curl -s https://fr.finance.yahoo.com/quote/TSLA/ | grep -oP '(?<=data-test="ONE_YEAR_TARGET_PRICE-value">)[\d,]+')
+target=$(curl -s https://fr.finance.yahoo.com/quote/TSLA/ | grep -oP '(?<=data-test="ONE_YEAR_TARGET_PRICE-value">)[\d,]+' | sed 's/,/./')
 
-#echo -e "Prev.Close : $previous_close \nOpen.Price : $ouverture \nCap.Boursière : $cap_boursiere \nVolume : $volume M \nTarget Price : $target"
-
+timestamp=$(date +%s)
 
 # Spécifiez le nom du fichier CSV
 CSV_FILE="report.csv"
@@ -19,7 +18,7 @@ CSV_FILE="report.csv"
 # Vérifiez si le fichier CSV est vide
 if [ ! -s "$CSV_FILE" ]; then
   # Ajoutez les noms des colonnes si le fichier est vide
-  echo "prev_close,open_price,cap_boursiere,volume,target" > $CSV_FILE
+  echo "timestamp,prev_close,open_price,cap_boursiere,volume,target" > $CSV_FILE
 fi
 
-echo $previous_close,$ouverture,$cap_boursiere,$volume,$target >> $CSV_FILE
+echo $timestamp,$previous_close,$ouverture,$cap_boursiere,$volume,$target >> $CSV_FILE
